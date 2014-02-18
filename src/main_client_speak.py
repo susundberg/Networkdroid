04b5c7e5512597d086bd7888538bf40863f9d65e
Pauli Sundberg <pauli.sundberg@iki.fi>
2014-02-18 18:10:33 +0200

    Make client speak

--- src/main_client_speak.py
+++ src/main_client_speak.py
@@ -7,6 +7,8 @@ import datetime
 
 from Sundberg.Logger import *
 
+from subprocess import call
+
 def get_command_line_arguments( ):
    parser = argparse.ArgumentParser(description='Speak-aloud espeak client')
    parser.add_argument("configfile", help = "Set configuration file to be used")
@@ -28,11 +30,17 @@ def main( args ):
 class SpeakClient:
   def __init__(self, config, log ):
      self.module_alives = {}
-     self.speak_lines   = { "PingAlive" : "Connection to server",
-                     "PingIP"    : "Connection to internet" }
+     self.speak_lines   = {
+                     "pingalive" : "Connection to droid server",
+                     "pingip"    : "Connection to internet",
+                     "pinghost"  : "Domain name server",
+                     "ssh"       : "Nat compromised" 
+                     }
+     
      self.timeout = int( config["client_speak_timeout"] )
      self.address = config["protocol"] + ":" + config["port_client_pub"]
      self.log     = log
+     self.speak_command = config["client_speak_cmd"]
      
   def mainloop(self):
      context = zmq.Context(1)
@@ -44,26 +52,39 @@ class SpeakClient:
      deadline = datetime.timedelta( milliseconds =  self.timeout )
    
      while( True ):
-       self.check_for_dead_modules( deadline )
+       # We need to first check if we have messages waiting, if yes, process those
+       # If not, do dead module check and enter timeout receive
+       # We need to do this to avoid a) dead modules check omitted b) dead modules check done while we have lines
+       # waiting to be processed (since the speak can take several secs)
+       
+       # First check if we have messages waiting
        try:
-         message = receive_socket.recv(  )
+         message = receive_socket.recv( flags = zmq.NOBLOCK )
+         self.process_message( message )
+         continue
        except zmq.ZMQError as error:
-         if error.errno == zmq.EAGAIN :
-            # Timeouted, check for dead modules and continue
-            continue
-         else :
+         if error.errno != zmq.EAGAIN :
             raise( error )
+            
+       self.check_for_dead_modules( deadline )     
+       # No messages ready, do timeout receive
+       try:
+          message = receive_socket.recv(  )
+          self.process_message( message )
+       except zmq.ZMQError as error:
+         if error.errno != zmq.EAGAIN :
+            raise( error )
+            
       
-       self.process_message( message )
    
    
   def process_message( self, message ): 
       fields = message.split(":")
       if len(fields) == 2 and fields[1].strip() == "HB":
-         module_name = fields[0].strip()
+         module_name = fields[0].strip().lower()
          if module_name in self.speak_lines: 
             if module_name not in self.module_alives:
-               speak_aloud( self.speak_lines[ module_name ] + " OK" )
+               self.speak_aloud( self.speak_lines[ module_name ] + " OK" )
                self.log.info("Module '%s' ONLINE" % module_name )
             self.module_alives[ module_name ] = datetime.datetime.now()
       else:
@@ -76,20 +97,19 @@ class SpeakClient:
      for module_name in self.module_alives:
         elapsed_since_last_hb = current_time - self.module_alives[ module_name ]
         if elapsed_since_last_hb > deadline:
-           speak_aloud( self.speak_lines[ module_name  ] + " got dropped")
+           self.speak_aloud( self.speak_lines[ module_name  ] + " got dropped")
            to_remove.append( module_name )
            self.log.info("Module '%s' went offline" % module_name )
      for module_name in to_remove:
            del self.module_alives[ module_name ]
 
-def speak_aloud( line ):
-   print "ESPEAK: " + line
-      
-   
+  def speak_aloud( self, line ):
+     retcode = call( [ self.speak_command, line ] )
+     if retcode != 0 :
+        self.log.error("Call '%s' returned nonzero: %d" % ( self.speak_command, retcode ) )
+     self.log.debug("Speak aloud: " + line )
 
-
-
-   
+  
 if __name__ == "__main__":
     sys.exit( main( get_command_line_arguments() ) )
   
